ba76518823544e50d7ba854b6701f09705a80b8a
Charles Alleman <charles@businessinmind.com.au>
2020-03-06 10:04:46 +1000

    Week 1 Solutions

---
+++ week2/src/Animal/animal.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function AnimalComponent({ name, number, eats }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{number}</p>
+      <p>Eats the following</p>
+      <ul>
+        {eats.map(food => (
+          <li>{food}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ week2/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+import AnimalComponent from "./Animal/animal";
+
+export default function App() {
+  const animals = [
+    {
+      name: "Lion",
+      number: 3,
+      eats: ["zebra", "antelope", "buffalo", "hippopotamus"]
+    },
+    {
+      name: "Tiger",
+      number: 5,
+      eats: ["moose", "deer", "buffalo"]
+    },
+    {
+      name: "Giraffe",
+      number: 6,
+      eats: ["leaves", "twigs"]
+    },
+    {
+      name: "Elephant",
+      number: 4,
+      eats: ["grass", "leaves", "flowers", "fruit"]
+    },
+    {
+      name: "Monkey",
+      number: 10,
+      eats: ["fruit", "leaves", "vegetables", "insects"]
+    },
+    {
+      name: "Lemur",
+      number: 15,
+      eats: ["fruit", "leaves", "insect"]
+    },
+    {
+      name: "Rhinoceros",
+      number: 2,
+      eats: ["grass", "shoots", "leaves", "berries"]
+    }
+  ];
+
+  return (
+    <div className="App">
+      <h1>Hello CAB230</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      {animals.map(animal => (
+        <AnimalComponent {...animal} />
+      ))}
+    </div>
+  );
+}
