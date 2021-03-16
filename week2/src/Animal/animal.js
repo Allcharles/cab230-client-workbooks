102dc4f8eb1f215e1fee09689eb5027290a37a9a
Charles Alleman <charles@businessinmind.com.au>
2021-03-16 10:34:43 +1000

    Update week 2

--- week2/src/Animal/animal.js
+++ week2/src/Animal/animal.js
@@ -1,16 +1,11 @@
-/**
- * IMPORTANT Import this whenever you wish to create a react component
- */
-import React from "react";
-
-export default function AnimalComponent({ name, number, eats }) {
+export function AnimalComponent({ name, number, eats }) {
   return (
     <div>
       <h1>{name}</h1>
       <p>{number}</p>
       <p>Eats the following</p>
       <ul>
-        {eats.map(food => (
+        {eats.map((food) => (
           <li>{food}</li>
         ))}
       </ul>
--- week2/src/App.js
+++ week2/src/App.js
@@ -1,44 +1,44 @@
 import React from "react";
 import "./App.css";
-import AnimalComponent from "./Animal/animal";
+import { AnimalComponent } from "./Animal/animal";
 
 export default function App() {
   const animals = [
     {
       name: "Lion",
       number: 3,
-      eats: ["zebra", "antelope", "buffalo", "hippopotamus"]
+      eats: ["zebra", "antelope", "buffalo", "hippopotamus"],
     },
     {
       name: "Tiger",
       number: 5,
-      eats: ["moose", "deer", "buffalo"]
+      eats: ["moose", "deer", "buffalo"],
     },
     {
       name: "Giraffe",
       number: 6,
-      eats: ["leaves", "twigs"]
+      eats: ["leaves", "twigs"],
     },
     {
       name: "Elephant",
       number: 4,
-      eats: ["grass", "leaves", "flowers", "fruit"]
+      eats: ["grass", "leaves", "flowers", "fruit"],
     },
     {
       name: "Monkey",
       number: 10,
-      eats: ["fruit", "leaves", "vegetables", "insects"]
+      eats: ["fruit", "leaves", "vegetables", "insects"],
     },
     {
       name: "Lemur",
       number: 15,
-      eats: ["fruit", "leaves", "insect"]
+      eats: ["fruit", "leaves", "insect"],
     },
     {
       name: "Rhinoceros",
       number: 2,
-      eats: ["grass", "shoots", "leaves", "berries"]
-    }
+      eats: ["grass", "shoots", "leaves", "berries"],
+    },
   ];
 
   return (
@@ -46,7 +46,7 @@ export default function App() {
       <h1>Hello CAB230</h1>
       <h2>Start editing to see some magic happen!</h2>
 
-      {animals.map(animal => (
+      {animals.map((animal) => (
         <AnimalComponent {...animal} />
       ))}
     </div>
